d3193ba2b7b865569aea3591f4f8d8c70f1fa02e
Ezequias Torczuk <144610780+MrDadoxx@users.noreply.github.com>
2024-12-06 16:55:02 -0300

    CardDisplay testing file added (w/ Vitest)

---
+++ src/components/CardDisplay.test.tsx
@@ -0,0 +1,38 @@
+import { describe, it, expect } from 'vitest'
+import { render } from '@testing-library/react'
+import { Card } from '../models/Card.ts'
+import CardDisplay from './CardDisplay.tsx'
+import { Agents } from '../services/Agents.service.ts'
+import { Agent } from '../models/Agent.ts'
+
+test('o', async () => {
+  const agents: Agent[] = await Agents.getByName(['Omen'])
+  const card: Card = Agents.getCardsFromAgents(agents)[0]
+
+  const component = render(<CardDisplay card={card} />)
+  expect(component.container).toHaveTextContent(card.name)
+})
+
+describe('CardDisplay', async () => {
+  const agents: Agent[] = await Agents.getByName(['Omen'])
+  const card: Card = Agents.getCardsFromAgents(agents)[0]
+
+  it(`Shouldn't show the card level`, () => {
+    const component = render(<CardDisplay card={card} showLevel={false} />)
+
+    expect(component.container).not.toHaveTextContent(String(card.level))
+  })
+
+  it(`Should show the card name`, () => {
+    const component = render(<CardDisplay card={card} />)
+
+    expect(component.container).toHaveTextContent(card.name)
+  })
+
+  it(`Should show the card image`, () => {
+    const component = render(<CardDisplay card={card} />)
+
+    expect(component.getByAltText('Agent Image')).toHaveAttribute('src', card.image)
+
+  })
+})
